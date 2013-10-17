52fb9f073607c1a41086f8c2c030b7f76336880d
jimmyzhong <382766860@qq.com>
2013-10-17 13:23:28 +0800

    Update pay.js

--- plugin/pay.js
+++ plugin/pay.js
@@ -1,5 +1,5 @@
 (function() {
   var jsonrpc_version = '2.0';
-	console("jsonrpc_version:" + jsonrpc_version);
+	console.log("jsonrpc_version:" + jsonrpc_version);
 	document.body.style.backgroundColor="#FFFFCE";
 })();
